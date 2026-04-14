765780ece1d2ae2c913cd2164a473f7c16c921d9
MartinScepka <martin.scepkait@gmail.com>
2026-04-14 13:37:05 +0200

    Gui implementation

--- src/main/java/cz/uhk/timetable/utils/impl/MockTimetableProvider.java
+++ src/main/java/cz/uhk/timetable/utils/impl/MockTimetableProvider.java
@@ -15,9 +15,12 @@ public class MockTimetableProvider implements TimetableProvider {
     public LocationTimetable read(String building, String room) {
         var tt = new LocationTimetable(building, room);
         var activities = List.of(
-            new Activity("PRO1", "Programovani 1", "utery", LocalTime.of(12,25), LocalTime.of(13, 55), "Kozel"),
-            new Activity("OS1A", "Operacni Systemy 1", "streda", LocalTime.of(14,50), LocalTime.of(16, 25), "Almer"),
-            new Activity("ZMI2", "Zaklady Matematiky 2", "pondeli", LocalTime.of(11,35), LocalTime.of(13, 10), "Bauer")
+            new Activity("PRO1", "Programovani 1", "utery",
+                    LocalTime.of(12,25), LocalTime.of(13, 55), "Kozel"),
+            new Activity("OS1A", "Operacni Systemy 1", "streda",
+                    LocalTime.of(14,50), LocalTime.of(16, 25), "Almer"),
+            new Activity("ZMI2", "Zaklady Matematiky 2", "pondeli",
+                    LocalTime.of(11,35), LocalTime.of(13, 10), "Bauer")
         );
         tt.setActivities(activities);
         return tt;
